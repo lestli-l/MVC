b4d99382979c684435a15e82f508fd78bc420a28
lestli-l <45630439+lestli-l@users.noreply.github.com>
2020-04-10 15:51:38 +0800

    添加项目文件。

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Manage_core.Data;
+using Manage_core.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+using Microsoft.AspNetCore.Authorization;
+
+
+
+namespace Manage_core.Controllers
+{ 
+    public class UsersController : Controller
+    {
+        private readonly UserContext _context;
+
+        public UsersController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string name,string password){
+            if (name.Equals("admin@q") && password.Equals("123456"))
+
+            {
+
+                var claims = new List<Claim>(){
+
+         new Claim(ClaimTypes.Name,name),new Claim("password",password)
+
+};
+
+                var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims, "Customer"));
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal, new AuthenticationProperties
+
+                {
+
+                    ExpiresUtc = DateTime.UtcNow.AddMinutes(20),
+
+                    IsPersistent = false,
+
+                    AllowRefresh = false
+
+                });
+               var user= await _context.User.ToListAsync();
+                return View("Index",user);
+
+            }
+
+            return Json(new { result = false, msg = "用户名密码错误!" });
+
+
+        }
+
+    }
+}
---
+++ Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Manage_core.Models;
+
+namespace Manage_core.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Manage_core.Models.User> User { get; set; }
+    }
+}
